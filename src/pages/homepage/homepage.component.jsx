aae04629db6cde2e150eae9ac435da5692bad99d
Ahmad Raza Soft.Engr <ahmad.raza916@gmail.com>
2020-04-01 13:35:16 +0500

    new updates

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Directory from '../../component/directory/directory.component';
+
+const HomePage = () => (
+<div className="home-page">
+
+        <Directory />
+  
+    </div>
+
+);
+
+export default HomePage;
